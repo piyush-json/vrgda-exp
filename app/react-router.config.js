ef2a747750185039817294f8298d8a93be7e41b4
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2025-06-16 14:57:28 +0530

    ts fix

--- app/react-router.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    // Config options...
-    // Server-side render by default, to enable SPA mode set this to `false`
-    ssr: false
-};
